f5d685f0b27cc2ad66f2fa159b1831d741c68765
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-09-03 13:40:58 +0700

    Create adapter for ProductCategory Model

---
+++ app/src/main/java/com/rujirakongsomran/ecommerceapp/adapter/ProductAdapter.java
@@ -0,0 +1,53 @@
+package com.rujirakongsomran.ecommerceapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rujirakongsomran.ecommerceapp.databinding.ProductRowItemBinding;
+import com.rujirakongsomran.ecommerceapp.model.ProductCategory;
+
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHolder> {
+
+    Context context;
+    List<ProductCategory> productCategoryList;
+
+    public ProductAdapter(Context context, List<ProductCategory> productCategoryList) {
+        this.context = context;
+        this.productCategoryList = productCategoryList;
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ProductViewHolder(ProductRowItemBinding.inflate(LayoutInflater.from(parent.getContext()),
+                parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        holder.binding.tvCategoryName.setText(productCategoryList.get(position).getProductName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return productCategoryList.size();
+    }
+
+    public static final class ProductViewHolder extends RecyclerView.ViewHolder {
+
+        ProductRowItemBinding binding;
+
+        public ProductViewHolder(@NonNull ProductRowItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+}
